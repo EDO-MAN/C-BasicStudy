0d5e6de7321387cb7350f59c915a8965eada429c
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-07-06 17:58:12 +0900

    6장까지 완료
    다음은 7장 클래스부터 시작

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+        //열거형식 enum
+        enum DialogResult
+        {
+            YES,
+            NO,
+            CANCEL,
+            CONFIRM,
+            OK
+        }
+        public static int Fibonacci(int n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            //2장 연습문제
+            WriteLine("여러분 안녕하세요?");
+            WriteLine("반갑습니다!");
+            //5장 연습문제
+            //for(int i = 0; i < 5; i++)
+            //{
+            //    for(int j = 0; j <= i; j++)
+            //    {
+            //        Write("*");
+            //    }
+            //    WriteLine();
+            //}
+            //역계단
+            //for (int i = 5; i > 0; i--)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Write("*");
+            //    }
+            //    WriteLine();
+            //}
+            int result = Fibonacci(5);
+            WriteLine($"{result}");
+            int a = 20;
+            int b = 3;
+
+            Divide(a, b, out int c, out int d);
+
+            WriteLine($"a:{a}, b : {b}, a/b : {c}, a % b : {d}");
+
+            int sum = Sum(3,4,5,6,7,8,9,10);
+
+            WriteLine($"{sum}");
+        }
+        static int Sum(params int[] args)
+        {
+            Write("Summing...");
+
+            int sum = 0;
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                if (i > 0)
+                    Write(",");
+
+                Write(args[i]);
+
+                sum += args[i];
+            }
+            WriteLine();
+
+            return sum;
+        }
+    }
+}
---
+++ HelloWorld/MainApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, mean);
+
+            WriteLine($"{mean}");
+
+        }
+        public static void Mean(double a, double b, double c, double d, double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
