ab532c069018b606d910d2c36261774c7243cffb
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-07-12 23:41:34 +0900

    8장 완료

---
+++ HelloWorld/ExtensionMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtension;
+
+namespace MyExtension
+{
+    public static class IntegerExtension 
+    {
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int myInt, int exponent)
+        {
+            int result = myInt;
+            for (int i = 1; i < exponent; i++)
+            {
+                result *= myInt;
+            }
+            return result;
+        }
+        public static string Append(this string myString, string exponent)
+        {
+            return myString + exponent;
+        }
+    }
+}
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            Console.WriteLine($"3^2 : {3.Square()}");
+            Console.WriteLine($"3^4 : {3.Power(4)}");
+            Console.WriteLine($"2^10 : {2.Power(10)}");
+            Console.WriteLine(hello.Append(",World!"));
+        }
+    }
+}
---
+++ HelloWorld/test1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class A
+    {
+        public void test1()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B
+    {
+        public void test2()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.test1();
+            B b = new B();
+            b.test2();
+        }
+    }
+}
