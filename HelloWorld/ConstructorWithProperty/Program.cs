c026528d08d0f2b1e20527dba499fa2a37bd3dff
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-07-13 01:39:21 +0900

    9장 프로퍼티 무명 형식까지

---
+++ HelloWorld/ConstructorWithProperty/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorWithProperty
+{
+    class BirthInfo
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthInfo birth = new BirthInfo()
+            {
+                Name = "서현";
+                
+            }
+        }
+    }
+}
