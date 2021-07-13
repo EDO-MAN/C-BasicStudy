86e6cad1ffd77381f7c5e4941a0dd35b20eeedbb
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-07-13 20:29:17 +0900

    10장 배열 진행

---
+++ HelloWorld/Ex9_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9_1
+{
+    class NameCard
+    {
+        private int age;
+        private string name;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard namecard = new NameCard();
+            namecard.Age = 24;
+            namecard.Name = "이도현";
+
+            Console.WriteLine($"{namecard.Age}");
+            Console.WriteLine($"{namecard.Name}");
+        }
+    }
+}
---
+++ HelloWorld/PropertiesInterface/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesInterface
+{
+    interface INameValue
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+        string Value
+        {
+            get;
+            set;
+        }
+    }
+    class NameValue : INameValue
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameValue name = new NameValue()
+            {
+                Name = "이름",
+                Value = "박상현"
+            };
+
+            NameValue heihgt = new NameValue()
+            {
+                Name = "키",
+                Value = "177cm"
+            };
+
+            NameValue weight = new NameValue()
+            {
+                Name = "몸무게",
+                Value = "90kg"
+            };
+            Console.WriteLine($"{name.Name} : {name.Value}");
+            Console.WriteLine($"{heihgt.Name} : {heihgt.Value}");
+            Console.WriteLine($"{weight.Name} : {weight.Value}");
+        }
+    }
+}
